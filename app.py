f5517990fcc940cffb70e401c89659a65069b5e1
samik-boop <m84samik@gmail.com>
2026-04-16 21:32:55 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 @app.route ("/health")
 def health():
   return {"status": "ok"}
-@app.route ("/")
+@app.route("/")
 def home():
-  return "Secure App Running"
+    return render_template("index.html")
 if __name__== "__main__":
   app.run(host="0.0.0.0", port=5000)
